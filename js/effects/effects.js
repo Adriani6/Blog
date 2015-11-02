303a9e56def1b06102bfb69334f0821e20f25c56
unknown <1502689@RG43002.rgu.ac.uk>
2015-11-02 15:33:30 +0000

    Added Bootstrap & Converted the index to utilise it.

--- js/effects/effects.js
+++ js/effects/effects.js
@@ -5,19 +5,20 @@
 $(document).ready(function(){
     $(".nav_top_content").click(function(){
         $(".login").slideDown("slow");
+		$(".login").css("display", "table");
         $(".nav_top").hide("slow");
 
     });
 
     $("body").click(function(e){
         if(!$(e.target).is(".login")) {
-            if(!$(e.target).is("#field")){
+            if(!$(e.target).is("#input")){
                 if ($(".nav_top").is(":hidden")) {
                     $(".login").hide("slow");
                     $(".nav_top").slideDown("slow");
                 }
             }
-
         }
     });
+	
 });
