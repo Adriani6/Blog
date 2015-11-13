ebf856e970edacffd6ec31c50d9682b882f42755
Adriani6 <adrian@adriani6.co.uk>
2015-11-13 15:05:20 +0000

    Testing MySQL.
    Signed-off-by: Adriani6 <adrian@adriani6.co.uk>

--- utils/mysql.php
+++ utils/mysql.php
@@ -4,10 +4,17 @@ $user = "b3216f07d20ee7";
 $pass = "d597404f";
 $db = "blog-db";
 
-$connection = new mysqli($server, $user, $pass);
+$connection = new mysqli_connect($server, $user, $pass, $db);
 
 if($connection->connection_error){
 	die("Connection Failed.")
 }
+mysqli_query($connection, "
+CREATE TABLE Users(
+user_id int Primary Key,
+username varchar(30) not null,
+password varchar(30) not null,
+)
 
+");
 ?>
