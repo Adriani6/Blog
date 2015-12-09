ffb602d315bf09303fc83d3af12425a8de043bed
RobertMcateer <bob@robertmcateer.com>
2015-12-09 14:19:10 +0000

    Adding a cookie usage alert.

--- site_body.php
+++ site_body.php
@@ -38,13 +38,12 @@ if(isset($_SESSION['user'])){
     <script src="js/html_utils.js"></script>
     <script src="js/effects/effects.js"></script>
 
-    <script type="text/javascript" src="js/jquery.cookieBar.js"></script>
-    <link rel="stylesheet" href="css/jquery.cookieBar.css">
+    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
     <script type="text/javascript">
-        $(document).ready(function(){
-            $.cookieBar();
-        });
+        window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"https://www.cookielaw.org/the-cookie-law/","theme":"dark-floating"};
     </script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
+    <!-- End Cookie Consent plugin -->
 
 
     <!--[if lt IE 9]>
