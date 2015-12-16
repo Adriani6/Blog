18cc39db1e64815af22a42c0b4207faf6b16e8f6
Adriani6 <adrian@adriani6.co.uk>
2015-12-16 16:28:08 +0000

    Attempt to fix scripts which do not load after x amount of seconds.
    Signed-off-by: Adriani6 <adrian@adriani6.co.uk>

--- site_body.php
+++ site_body.php
@@ -47,29 +47,8 @@ function __autoload($class){
     <link rel="stylesheet" href="css/adventure_body.css">
     <link rel="stylesheet" href="css/profile.css">
 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-    <script src="bootstrap_js/bootstrap.min.js"
-            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-            crossorigin="anonymous"></script>
-    <script src="js/html_utils.js"></script>
-    <script src="js/effects/effects.js"></script>
-
-    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
-    <script type="text/javascript">
-    //    window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"https://www.cookielaw.org/the-cookie-law/","theme":"light-floating"};
-    </script>
-    <!--<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
-    <!-- End Cookie Consent plugin -->
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-	<script>
-	$(document).ready(function(){
-		$('.dropdown-toggle').dropdown()
-		$("#calendar").click(function(){
-			$("#calendar").datepicker();
-		});
-	});
-	</script>
+
 	<script>
 	//This sets the toolbar to stay on the page while scrolling, there's a bug with positioning...
 	/*
