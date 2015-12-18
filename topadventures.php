4b21927561dcd9be9cca45fee0ccb9dd95ec013a
Adrian Iwaszkiewicz <adrian@adriani6.co.uk>
2015-12-18 16:56:13 +0000

    Fixed up W3C
    Signed-off-by: Adrian Iwaszkiewicz <adrian@adriani6.co.uk>

--- topadventures.php
+++ topadventures.php
@@ -3,21 +3,26 @@
 	foreach($adventures as $adve){
 		$pictures = $adve['Images'];
 		//var_dump($adv->getScore($adve['ID']));
-	echo "
-	<div class='panel panel-default' style='margin-top: 10px;'>
-		<div class='panel-heading' style='background-color: orange;'>
-			<a href='adventure.php?id={$adve['ID']}'>{$adve['Title']} (Rating: {$adve['Rating']})</a>
-			<span style='float:right;' class='glyphicon glyphicon-user'><a href='profile.php?user={$adve['User']}'>{$user->getUsernameFromID($adve['User'])}</a></span>
-		</div>
-		<div class='panel-body'>
-			<span style='float:right;' class='glyphicon glyphicon-globe'>".$a->getCountryNameById($adve['Country'])."</span><hr />
-			<div class='row'><div class='col-md-6 col-md-offset-3'><img src='{$adve['MainPicture']}' height='300' width='450'></div></div>
-			<h4>Description</h4>
-			<div class='well well-sm'>
-				<span>{$adve['ShortDescription']}...</span>
-			</div>
-		</div>
-	</div>
-	";
 		
-	}?>
+			echo "
+			<div class='panel panel-default' style='margin-top: 10px;'>
+				<div class='panel-heading' style='background-color: orange;'>
+					<a href='adventure.php?id={$adve['ID']}'>{$adve['Title']} (Rating: {$adve['Rating']})</a>
+					<span style='float:right;' class='glyphicon glyphicon-user'><a href='profile.php?user={$adve['User']}'>{$user->getUsernameFromID($adve['User'])}</a></span>
+				</div>
+				<div class='panel-body'>
+					<span style='float:right;' class='glyphicon glyphicon-globe'>".$a->getCountryNameById($adve['Country'])."</span><hr />
+					<div class='row'>"; 
+					if(!empty($adve['MainPicture'])){
+						echo"<div class='col-md-6 col-md-offset-3'><img src='{$adve['MainPicture']}' alt='{$adve['MainPicture']}' height='300' width='450'></div>
+					</div>"; } echo"
+					<h4>Description</h4>
+					<div class='well well-sm'>
+						<span>{$adve['ShortDescription']}...</span>
+					</div>
+				</div>
+			</div>
+			";
+		
+	}
+	?>
